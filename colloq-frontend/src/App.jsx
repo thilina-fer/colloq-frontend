782e7fe03881be92b31f4f8f0009a0c7b7ef0ef2
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-08 08:49:34 +0530

    feat : create a logo component

--- colloq-frontend/src/App.jsx
+++ colloq-frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import DottedBackground from "./components/backgroundEffect";
+import ColloQLogo from "./components/logo";
 
 function App() {
-  return <DottedBackground />;
+  return (
+    <>
+      <DottedBackground />
+      <ColloQLogo />  
+    </>
+  );
 }
 
 export default App;
